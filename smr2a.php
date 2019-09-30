b945df441366aeeed6d4882b9a7e71896d877e91
Alberto-cloud <albertorosamartinez8@gmail.com>
2019-09-30 20:08:19 +0200

    php

---
+++ smr2a.php
@@ -0,0 +1,8 @@
+<a href="http://192.168.0.110/smr2A_Jose/smr2a.php">Jose</a>
+Alberto <a href="http://192.168.0.203/SMR2A_ADRIAN/smr2a.php">Adrian</a>
+<?php
+
+$f=fopen('visitas.txt','a');
+fwrite($f,$_SERVER['REMOTE_ADDR']."\r\n");
+fclose($f);
+?>
