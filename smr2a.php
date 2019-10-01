2bf28ba8b3904bbc71b38c312f9c1d742bdad708
Alberto-cloud <albertorosamartinez8@gmail.com>
2019-10-01 19:40:52 +0200

    formulario

--- smr2a.php
+++ smr2a.php
@@ -1,8 +1,29 @@
 <a href="http://192.168.0.110/smr2A_Jose/smr2a.php">Jose</a>
 Alberto <a href="http://192.168.0.203/SMR2A_ADRIAN/smr2a.php">Adrian</a>
-<?php
 
+<?php
 $f=fopen('visitas.txt','a');
+fwrite($f,date('Y-m-d H:i'));
 fwrite($f,$_SERVER['REMOTE_ADDR']."\r\n");
 fclose($f);
 ?>
+
+<?php
+if(isset($_GET['correo'])){
+	$f=fopen('suscriptores.txt','a');
+	fwrite($f,$_GET['correo']."\r\n");
+	fclose($f);
+}
+?>
+
+
+<html>
+<body>
+
+<form>
+    Email: <input type="email" name="correo"><br>
+    <button>Enviar</button>
+</form>
+
+</body>
+</html>
