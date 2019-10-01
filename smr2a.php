32ca4d9264a3fc34c52d9e2bbcbfcb2a80ffdc73
Alberto-cloud <albertorosamartinez8@gmail.com>
2019-10-01 20:21:05 +0200

    catalogo

--- smr2a.php
+++ smr2a.php
@@ -1,10 +1,10 @@
 <a href="http://192.168.0.110/smr2A_Jose/smr2a.php">Jose</a>
 Alberto <a href="http://192.168.0.203/SMR2A_ADRIAN/smr2a.php">Adrian</a>
-<hmtl>
+<html>
 	<h1>
 		<a href="http://localhost/smr2a_alberto/catalogo.php">LISTA GITHUB</a>
 	</h1>
-	</hmtl>
+	</html>
 <?php
 $f=fopen('visitas.txt','a');
 fwrite($f,date('Y-m-d H:i'));
