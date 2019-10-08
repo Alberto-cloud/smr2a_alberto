78db1e9600837c3b02a67361f76aa80e9ac84a08
Alberto-cloud <albertorosamartinez8@gmail.com>
2019-10-08 20:27:17 +0200

    Bucles

---
+++ 2e.php
@@ -0,0 +1,27 @@
+<?php
+function calcular($a){
+
+
+	echo "<p>$a x 1 ="; 			echo ($a*1).'</p>';
+	echo "<p>$a x 1 =";				echo ($a*2).'</p>';
+	echo "<p>$a x 1 =";				echo ($a*3).'</p>';
+	echo "<p>$a x 1 =";				echo ($a*4).'</p>';
+	echo "<p>$a x 1 =";				echo ($a*5).'</p>';
+	echo "<p>$a x 1 =";				echo ($a*6).'</p>';
+	echo "<p>$a x 1 =";				echo ($a*7).'</p>';
+	echo "<p>$a x 1 =";				echo ($a*8).'</p>';
+	echo "<p>$a x 1 =";				echo ($a*9).'</p>';
+	echo "<p>$a x 1 =";				echo ($a*10).'</p>';
+}
+calcular(1);
+calcular(2);
+calcular(3);
+calcular(4);
+calcular(5);
+calcular(6);
+calcular(7);
+calcular(8);
+calcular(9);
+calcular(10);
+
+?>
