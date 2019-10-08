74a9faa4142665a92466d5b0080c4e697fc45427
Alberto-cloud <albertorosamartinez8@gmail.com>
2019-10-08 19:33:50 +0200

    Condicionales

--- 2d.php
+++ 2d.php
@@ -3,9 +3,12 @@ function calcula($a,$b){
 echo ($a+$b).'<br>';
 echo ($a-$b).'<br>';
 echo ($a*$b).'<br>';
-echo ($a/$b).'<br>';
+if($b!=0) echo ($a/$b).'<br>';
 }
-calcula(9,15);
+calcula(9,0);
+
+
+
 
 
 ?>
