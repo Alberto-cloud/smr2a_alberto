bf3625aa86196b87c2519ecf2789109ebdabc50a
Alberto-cloud <albertorosamartinez8@gmail.com>
2019-11-04 20:25:23 +0100

    HTML.PHP

---
+++ html.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<title>
+	Se tenía que decir y se dijo
+	</title>
+	</head>
+			
+	
+	<body bgcolor="#E6E6FA">
+	<center>
+									<table border="2px"> 
+										<tr>
+										<td>
+										<center>
+	<h1>Se tenía que decir y se dijo</h1>
+		<form>
+			Si me ha faltado alguna comunidad autónoma,escríbala por favor <input type="text" name="texto"><br>
+					<button>Enviar</button>
+		</form>
+		<img src="dijo.jpg"  width=”250” height=”80”/>
+	<h2>
+		<u>Comunidades autónomas bañadas por el Cantábrico</u>
+	</h2>
+	<font size="14" color="blue">Estas son las comunidades autónomas bañadas por el Cantábrico</font>
+																<table border="2px"> <!-- Lo cambiaremos por CSS -->
+																<tr>
+																<td>Celda 1</td>
+																<td>Celda 2</td>
+																<td>Celda 3</td>
+																</tr>
+																<tr>
+																<td>Celda 4</td>
+																<td>Celda 5</td>
+																<td>Celda 6</td>
+																</tr>
+																</table>
+										</center>
+										</td>
+										</tr>
+									</table>
+	</center>
+	</body>
+</html>
