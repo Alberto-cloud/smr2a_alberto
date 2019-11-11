2680826ee1f39062e13ac54cfb011d5370887ef9
Alberto-cloud <albertorosamartinez8@gmail.com>
2019-11-11 20:12:54 +0100

    html.php

--- html.php
+++ html.php
@@ -13,37 +13,39 @@
 										<tr>
 										<td>
 										<center>
-	<h1>Se tenía que decir y se dijo</h1>
-		<form>
-			<p><h5>Si me ha faltado alguna comunidad autónoma,escríbala por favor</h5></p> <input type="text" name="texto"><br>
-					<p><button type='submit'>Enviar</button></p>
-		</form>
-		<img src="dijo.jpg"  width=”250” height=”80”/>
-	<h2>
-		<u>Comunidades autónomas bañadas por el Cantábrico</u>
-	</h2>
-	<font size="14" color="#F781F3">Estas son las comunidades autónomas bañadas por el Cantábrico</font>
-				<table border="2px"> 
+	<h1><font size="14" color="#ff94b2">LA PELÍCULA</font></h1>
+	<h2><font size="10" color="#c894ff">Se tenía que decir y se dijo</font></h2>
+		
+		<img src="dijo.jpg"  width="400" height="250"/>
+		<br/><font size="9" color="#ff94ae">Nunca en cines</font>
+	<h3><font size="10" color="#F781F3">Comunidades autónomas bañadas por el Cantábrico</font>
+	</h3>
+				<table border> 
 				<tr>
-				<td><b>País Vasco</b></td>
+				<td><h4><b>País Vasco</b></h4></td>
 				<td>Vizcaya y Guipúzcua</td>
 				</tr>
 						<tr>
-						<td><b>Cantabria</b></td>
+						<td><h4><b>Cantabria</b></h4></td>
 						<td>Santander</td>
 						</tr>
 								<tr>
-								<td><b>Asturias</b></td>
+								<td><h4><b>Asturias</b></h4></td>
 								<td>Gijón,Áviles,Sueve,Oriente y Eo-Navia</td>
 								</tr>
 										<tr>
-										<td><b>Galicia</b></td>
+										<td><h4><b>Galicia</b></h4></td>
 										<td>La Coruña y Lugo</td>
 										</tr>
 										</table>
+										<form>
+			<p><h5><font size="5">Si me ha faltado alguna comunidad autónoma,escríbala por favor</font></h5></p> <input type="text" name="texto"><br>
+					<p><button type='submit'>Enviar</button></p>
+		</form>
 																
-		<br>Hecho por Alberto Rosa</br>
-		<br>SMR2A Aplicaciones Web</br>
+		<br/>Hecho por Alberto Rosa
+		<br/>SMR2A Aplicaciones Web
+		
 										</center>
 										</td>
 										</tr>
