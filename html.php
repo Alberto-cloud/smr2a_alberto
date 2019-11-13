ee7f58cba4f00c382c7eb508eb9020ca561cf6b3
Alberto-cloud <albertorosamartinez8@gmail.com>
2019-11-13 16:24:14 +0100

    PáginaWeb Again

--- html.php
+++ html.php
@@ -9,15 +9,30 @@
 	
 	<body bgcolor="#FFCCCC">
 	<center>
-									<table border="2px"> 
+									<table width="80%" border="2px">
+											
 										<tr>
 										<td>
 										<center>
-	<h1><font size="14" color="#ff94b2"><u>LA PELÍCULA</u></font></h1>
-	<h2><font size="10" color="#c894ff">Se tenía que decir y se dijo</font></h2>
+	<h1><font size="14" color="#ff94b2"><u>PELÍCULAS DE ESTRENO</u></font></h1>
+	<h1><font size="10" color="#c894ff">Se tenía que decir y se dijo</font></h1>
 		
-		<img src="dijo.jpg"  width="400" height="250"/>
+			<img src="dijo.jpg"  width="400" height="250"/>
+			<p><h2><font  color="#ff94ae">Fecha  de estreno</h2></p></font>
+			15/7/2069
+			<p><h2><font  color="#ff94ae">Sinopsis</h2></p></font>
+			El pollo Alfredo, se embarcará
+			
+			
+		<h1><font size="10" color="#c894ff">Uganda Knuckcles KNOES DA WAE!</font>
+			<p><img src="uganda.jpg"  width="400" height="250"/></p>
+			<p><h2><font  color="#ff94ae">Fecha  de estreno</h2></p></font>
+			1/9/2039
+			<p><h2><font  color="#ff94ae">Sinopsis</h2></p></font>
+			
+			
 		<br/><font size="9" color="#ff94ae">Nunca en cines</font>
+		
 	<h3><font size="10" color="#F781F3">Comunidades autónomas bañadas por el Cantábrico</font>
 	</h3>
 				<table border> 
@@ -39,11 +54,11 @@
 										</tr>
 										</table>
 										<form>
-			<p><h5><font size="5">Si me ha faltado alguna comunidad autónoma,escríbala por favor</font></h5></p> <input type="text" name="texto"><br>
+			<p><h5><font size="5">Si me ha faltado alguna comunidad autónoma,escríbala por favor</font></h5></p>  <input type="text" name="texto"><br>
 					<p><button type='submit'>Enviar</button></p>
 		</form>
 																
-		<br/>Hecho por Alberto Rosa
+		Hecho por Alberto Rosa
 		<br/>SMR2A Aplicaciones Web
 		
 										</center>
